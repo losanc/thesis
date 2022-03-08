19782cdbedced33d96fa7c4ebd6272153975d33d
Hantao Hui <hantao.hui@rwth-aachen.de>
2022-03-08 12:28:04 +0100

    added NeoHookean

--- mesh/src/energy.rs
+++ mesh/src/energy.rs
@@ -1,5 +1,6 @@
-use std::ops::Mul;
+use std::ops::{Mul, Sub};
 
+use autodiff::MyLog;
 use nalgebra::{ClosedAdd, ClosedMul, ClosedSub, SMatrix, SVector, Scalar};
 use num::{One, Zero};
 
@@ -14,7 +15,9 @@ pub trait Energy<const L: usize, const D: usize> {
             + ClosedSub
             + Copy
             + Copy
-            + Mul<f64, Output = T>;
+            + MyLog
+            + Mul<f64, Output = T>
+            + Sub<f64, Output = T>;
 }
 
 struct StVenantVirchhoff<const DIM: usize> {
@@ -25,7 +28,16 @@ struct StVenantVirchhoff<const DIM: usize> {
 impl Energy<6, 2> for StVenantVirchhoff<2> {
     fn energy<T>(&self, vec: SVector<T, 6>, inv_mat: &SMatrix<T, 2, 2>, size: f64) -> T
     where
-        T: Scalar + Zero + One + ClosedAdd + ClosedMul + ClosedSub + Copy + Mul<f64, Output = T>,
+        T: Scalar
+            + Zero
+            + One
+            + ClosedAdd
+            + ClosedMul
+            + ClosedSub
+            + Copy
+            + MyLog
+            + Mul<f64, Output = T>
+            + Sub<f64, Output = T>,
     {
         let mat = nalgebra::matrix![
             vec[4]-vec[0], vec[2]-vec[0];
@@ -48,7 +60,16 @@ impl Energy<6, 2> for StVenantVirchhoff<2> {
 impl Energy<12, 3> for StVenantVirchhoff<3> {
     fn energy<T>(&self, vec: SVector<T, 12>, inv_mat: &SMatrix<T, 3, 3>, size: f64) -> T
     where
-        T: Scalar + Zero + One + ClosedAdd + ClosedMul + ClosedSub + Copy + Mul<f64, Output = T>,
+        T: Scalar
+            + Zero
+            + One
+            + ClosedAdd
+            + ClosedMul
+            + ClosedSub
+            + Copy
+            + MyLog
+            + Mul<f64, Output = T>
+            + Sub<f64, Output = T>,
     {
         let mat = nalgebra::matrix![
             vec[3]-vec[0], vec[6]-vec[0],vec[9]-vec[0];
@@ -78,66 +99,73 @@ struct NeoHookean<const DIM: usize> {
 impl Energy<6, 2> for NeoHookean<2> {
     fn energy<T>(&self, vec: SVector<T, 6>, inv_mat: &SMatrix<T, 2, 2>, size: f64) -> T
     where
-        T: Scalar + Zero + One + ClosedAdd + ClosedMul + ClosedSub + Copy + Mul<f64, Output = T>,
+        T: Scalar
+            + Zero
+            + One
+            + ClosedAdd
+            + ClosedMul
+            + ClosedSub
+            + Copy
+            + MyLog
+            + Mul<f64, Output = T>
+            + Sub<f64, Output = T>,
     {
-        todo!();
+        let mat = nalgebra::matrix![
+            vec[4]-vec[0], vec[2]-vec[0];
+            vec[5]-vec[1], vec[3]-vec[1];
+        ];
+        let matrix_f = mat * inv_mat;
+        let i1 = matrix_f.transpose() * matrix_f;
+        let i1 = i1.trace();
+        // let i2=  ?`
+        // Is i2 used in formula?
+
+        // i3 = matrix_f.determinate()
+        let i3 = matrix_f[(0, 0)] * matrix_f[(1, 1)] - matrix_f[(0, 1)] * matrix_f[(1, 0)];
+        let i3 = i3 * i3;
+        let logi3 = i3.myln();
+        let ene = (i1 - logi3 - 2.0) * (self.mu / 2.0) + logi3 * logi3 * (self.lambda / 8.0);
+        let ene = ene * (T::one() * size);
+        ene
     }
 }
 
-// macro_rules! neohookean_2d {
-//     ($vec:ident, $ene:ident,$mat:ident,$inv_mat:ident, $square:expr, $type:ty) => {
-//         let $mat = na::matrix![
-//                 $vec[4]-$vec[0], $vec[2]-$vec[0];
-//                 $vec[5]-$vec[1], $vec[3]-$vec[1];
-//             ];
-//         let matrix_f = $mat*$inv_mat;
-//         let i1 = matrix_f.transpose()*matrix_f;
-//         let i1 = i1.trace();
-//         // let i2=  ?
-//         // Is i2 used in formula?
-//         let i3 = matrix_f.determinate();
-//         let $ene = (i1-<$type>::one()*2.0-i3.log())*(MIU/2.0);
-//     };
-// }
+impl Energy<12, 3> for NeoHookean<2> {
+    fn energy<T>(&self, vec: SVector<T, 12>, inv_mat: &SMatrix<T, 3, 3>, size: f64) -> T
+    where
+        T: Scalar
+            + Zero
+            + One
+            + ClosedAdd
+            + ClosedMul
+            + ClosedSub
+            + Copy
+            + MyLog
+            + Mul<f64, Output = T>
+            + Sub<f64, Output = T>,
+    {
+        let mat = nalgebra::matrix![
+            vec[3]-vec[0], vec[6]-vec[0],vec[9]-vec[0];
+            vec[4]-vec[1], vec[7]-vec[1],vec[10]-vec[1];
+            vec[5]-vec[2], vec[8]-vec[2],vec[11]-vec[2];
+        ];
+        let matrix_f = mat * inv_mat;
+        let i1 = matrix_f.transpose() * matrix_f;
+        let i1 = i1.trace();
+        // let i2=  ?`
+        // Is i2 used in formula?
 
-// macro_rules! st_venant_virchhoff_2d {
-//     ($vec:ident, $ene:ident,$mat:ident,$inv_mat:ident, $square:expr, $type:ty) => {
-//         let $mat = na::matrix![
-//                 $vec[4]-$vec[0], $vec[2]-$vec[0];
-//                 $vec[5]-$vec[1], $vec[3]-$vec[1];
-//             ];
-//         let $mat = $mat*$inv_mat;
-//         let $mat  = ($mat.transpose() * $mat  -
-//         na::matrix![
-//             <$type>::one(), <$type>::zero();
-//             <$type>::zero(), <$type>::one();
-//         ]) *(<$type>::one()*0.5);
-
-//         let $ene = ($mat.transpose()*$mat).trace()*(<$type>::one()*MIU) +
-//          $mat.trace()*$mat.trace()*(<$type>::one()*(0.5*LAMBDA));
-//          let $ene = $ene *(<$type>::one()*$square);
-
-//     };
-// }
-
-// macro_rules! st_venant_virchhoff_3d {
-//     ($vec:ident, $ene:ident,$mat:ident,$inv_mat:ident, $square:expr, $type:ty) => {
-//         let $mat = nalgebra::matrix![
-//                 $vec[3]-$vec[0], $vec[6]-$vec[0],$vec[9]-$vec[0];
-//                 $vec[4]-$vec[1], $vec[7]-$vec[1],$vec[10]-$vec[1];
-//                 $vec[5]-$vec[2], $vec[8]-$vec[2],$vec[11]-$vec[2];
-//             ];
-//         let $mat = $mat*$inv_mat;
-
-//         let $mat  = ($mat.transpose() * $mat  -
-//         nalgebra::matrix![
-//             <$type>::one(), <$type>::zero(),<$type>::zero();
-//             <$type>::zero(), <$type>::one(),<$type>::zero();
-//             <$type>::zero(), <$type>::zero(),<$type>::one();
-//         ]) *(<$type>::one()*0.5);
-
-//         let $ene = ($mat.transpose()*$mat).trace()*(<$type>::one()*MIU) +
-//          $mat.trace()*$mat.trace()*(<$type>::one()*(0.5*LAMBDA));
-//          let $ene = $ene *(<$type>::one()*$square);
-//     };
-// }
+        // i3 = matrix_f.determinate()
+        let i3 = matrix_f[(0, 0)] * matrix_f[(1, 1)] * matrix_f[(2, 2)]
+            + matrix_f[(0, 1)] * matrix_f[(1, 2)] * matrix_f[(2, 0)]
+            + matrix_f[(0, 2)] * matrix_f[(1, 0)] * matrix_f[(2, 1)]
+            - matrix_f[(0, 2)] * matrix_f[(1, 1)] * matrix_f[(2, 0)]
+            - matrix_f[(0, 1)] * matrix_f[(1, 0)] * matrix_f[(2, 2)]
+            - matrix_f[(0, 0)] * matrix_f[(2, 1)] * matrix_f[(1, 2)];
+        let i3 = i3 * i3;
+        let logi3 = i3.myln();
+        let ene = (i1 - logi3 - 3.0) * (self.mu / 2.0) + logi3 * logi3 * (self.lambda / 8.0);
+        let ene = ene * (T::one() * size);
+        ene
+    }
+}
